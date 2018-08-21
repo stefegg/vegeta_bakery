63e8a45ee3d3010e11be4118d6b0831e31e1971f
Stef Egbert <stephenegbert@gmail.com>
2018-08-21 12:17:58 -0400

    hmm

--- server.rb
+++ server.rb
@@ -26,7 +26,7 @@ get '/contact' do
 end
 
 get '/events' do
-  @result = Curl::Easy.perform("https://www.eventbriteapi.com/v3/events/search//?q=baking&location.address=nyc&token=PIVETZD32CRELLWG4DJ5")
+  @result = Curl::Easy.perform("https://www.eventbriteapi.com/v3/events/search/?q=baking&location.address=nyc&token=44ZRBDN4HYITKNUPIVV7")
   @data = @result.body_str
   @parsed = JSON.parse(@data)
   @events = @parsed['events']
